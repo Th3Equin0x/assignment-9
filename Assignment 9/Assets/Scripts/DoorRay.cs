7af3bad86c9352c2814a12a79fa40b686ab9b2cc
Th3Equin0x <sgruver22@gmail.com>
2019-11-07 19:50:49 -0600

    Did the thing

---
+++ Assignment 9/Assets/Scripts/DoorRay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorRay : MonoBehaviour
+{
+    private Ray ray;
+    private RaycastHit hit;
+
+    private void Start()
+    {
+        ray.origin = this.transform.position + new Vector3(0, 0, 2);
+        ray.direction = Vector3.back;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(ray.origin, ray.direction * 3f);
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, 4f) && hit.rigidbody.gameObject.tag == "player")
+        {
+            Debug.Log("hit");
+            this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.back * 100);
+        }
+        else
+        {
+            //this.gameObject.GetComponent<Rigidbody>().force
+        }
+
+    }
+}
---
+++ Assignment 9/Assets/Scripts/PitTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitTrigger : MonoBehaviour
+{
+    public float strength;
+    private ForceMode forcemode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (this.tag == "Flare")
+            forcemode = ForceMode.Acceleration;
+        else if (this.tag == "Smoke")
+            forcemode = ForceMode.Force;
+        else if (this.tag == "Fire")
+            forcemode = ForceMode.Impulse;
+        else if (this.tag == "Steam")
+            forcemode = ForceMode.VelocityChange;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.tag == "Fire" || this.tag == "Steam")
+        {
+            Debug.Log("Force applied by Fire or Steam");
+            other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * strength * Time.deltaTime, forcemode);
+        }
+    }
+
+    private void OnTriggerStay (Collider other)
+    {
+        if (this.tag == "Flare" || this.tag == "Smoke")
+        {
+            Debug.Log("Force applied by Flare or Smoke");
+            other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * strength * Time.deltaTime, forcemode);
+        }
+    }
+}
